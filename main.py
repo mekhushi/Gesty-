66ec0e0ccf811d1319dc95ca1d56b3d1ea50132a
Khushi singh <145558409+mekhushi@users.noreply.github.com>
2025-06-27 11:46:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,32 +11,32 @@ tracker = HandTracker()
 gesture_history = deque(maxlen=6)
 last_performed = None
 last_time = 0
-cooldown = 2  # seconds
+cooldown = 2  
 
 try:
     while True:
         ret, frame = cap.read()
         if not ret:
-            print("⚠️ Camera error")
+            print(" Camera error")
             break
 
         landmarks = tracker.get_hand_landmarks(frame)
         gesture = detect_gesture(landmarks)
 
-        # Store gesture history for consistency
+        
         gesture_history.append(gesture)
         most_common = max(set(gesture_history), key=gesture_history.count)
 
-        # Confirm gesture only if stable for 5+ frames
+     
         if gesture_history.count(most_common) >= 5 and most_common:
             now = time.time()
             if most_common != last_performed or (now - last_time > cooldown):
-                print(f"✅ Confirmed: {most_common}")
+                print(f" Confirmed: {most_common}")
                 perform_action(most_common)
                 last_performed = most_common
                 last_time = now
 
-        # Visual
+       
         frame = tracker.draw_hand(frame)
         if most_common:
             cv2.putText(frame, f"Gesture: {most_common}", (10, 40),
